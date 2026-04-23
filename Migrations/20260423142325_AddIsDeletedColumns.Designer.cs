0556148bc0dc617413b939b67f1e360d8ed83912
Sevan <spidersevan@hotmail.com>
2026-04-23 17:24:26 +0300

    Add IsDeleted Columns for Database

--- Migrations/20260423142325_AddIsDeletedColumns.Designer.cs
+++ Migrations/20260423142325_AddIsDeletedColumns.Designer.cs
@@ -4,6 +4,7 @@ using CanvasRoomDesign.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CanvasRoomDesign.Migrations
 {
     [DbContext(typeof(CanvasDbcontext))]
-    partial class CanvasDbcontextModelSnapshot : ModelSnapshot
+    [Migration("20260423142325_AddIsDeletedColumns")]
+    partial class AddIsDeletedColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260423142325_AddIsDeletedColumns.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CanvasRoomDesign.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeletedColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Sections",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Halls",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "HallItems",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Groups",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Halls");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "HallItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Groups");
+        }
+    }
+}
